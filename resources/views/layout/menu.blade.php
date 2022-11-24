ef05d837d45823e8537ca26e8e30fa9f12c3ae04
kevinniel <k.niel.pro@gmail.com>
2022-11-24 10:21:17 +0100

    exemple crud

---
+++ resources/views/layout/menu.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li>
+            <a href="{{ route('book.index') }}" title="Liste des livres">Liste des livres</a>
+        </li>
+        <li>
+            <a href="{{ route('book.create') }}" title="Ajouter un livre">Ajouter un livre</a>
+        </li>
+    </ul>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BookController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/books', [BookController::class, 'index'])->name("book.index");
+Route::get('/create-book', [BookController::class, 'create'])->name("book.create");
+Route::post('/create-book', [BookController::class, 'store'])->name("book.store");
+Route::get('/book/{id}', [BookController::class, 'show'])->name("book.show");
+Route::get('/book/{id}/edit', [BookController::class, 'edit'])->name("book.edit");
+Route::put('/book/{id}', [BookController::class, 'update'])->name("book.update");
+Route::delete('/book', [BookController::class, 'destroy'])->name("book.destroy");
